403fe9d0fb4fdfd162307462331269c8e9915fcd
Moseskhan <34588306+Moseskhan@users.noreply.github.com>
2021-06-22 16:13:27 -0500

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,7 +1,7 @@
 let nums=[14,9,6,8, 11, 12, 23, 4, 3, 30];
 var compareNumber=10;
 const numl=nums.reduce(function(a,b,index, array){
-if(b>compareNumber || (a!=undefined && a>compareNumber)){
+if(b>compareNumber){
 array.splice(1);
  console.log(`number is larger ${a}${b}`);
  return index;
@@ -20,4 +20,5 @@ console.log(numl)
 //array=the array
 
 //null=initiliaze accumulator to null so that reducing starts from zero index
+//with no null=(a!=undefined && a>compareNumber) but the index will be wrong 
 

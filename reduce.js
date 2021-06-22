567f46f80d92fd212ebef765d00aebdc1c3a4632
Moseskhan <34588306+Moseskhan@users.noreply.github.com>
2021-06-22 16:06:58 -0500

    Create reduce.js

---
+++ reduce.js
@@ -0,0 +1,15 @@
+let nums=[14,9,6,8, 11, 12, 23, 4, 3, 30];
+var compareNumber=10;
+const numl=nums.reduce(function(a,b,index, array){
+if(b>compareNumber || (a!=undefined && a>compareNumber)){
+array.splice(1);
+ console.log(`number is larger ${a}${b}`);
+ return index;
+}else{
+console.log(`number is smaller ${a}${b}`);
+}
+
+
+}, null);
+
+console.log(numl)
